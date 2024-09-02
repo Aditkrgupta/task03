ffe82aa40b99d972d2eea29e8b51d88a0bb05ed6
Aditkrgupta <153991728+Aditkrgupta@users.noreply.github.com>
2024-09-03 00:38:48 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,66 @@
+console.log("Welcome to Tic-toe ")
+let turn = "X"
+let isgameover = false;
+const changeturn = () => {
+    return turn === "X" ? "0" : "X"
+}
+
+
+const checkwin = () => {
+    let boxtext = document.getElementsByClassName("boxtext")
+    let wins = [
+        [0,1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== ""))
+             {
+
+
+            if (turn === "X") {
+                document.querySelector(".info").innerText = "0" + " WON";
+            }
+            else {
+                document.querySelector(".info").innerText = "X" + " WON";
+            }
+
+            gameover = true;
+
+        }
+    })
+}
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector(".boxtext");
+    element.addEventListener("click", () => {
+        if (boxtext.innerText === '') {
+
+            boxtext.innerText = turn;
+            turn = changeturn();
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+
+                checkwin();
+            }
+        }
+    })
+
+})
+reset.addEventListener("click",()=>{
+    let boxtexts=document.querySelectorAll(".boxtext");
+    Array.from(boxtexts).forEach(element=>{
+        element.innerText=""
+    });
+    turn="X";
+    
+    
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+})
+
